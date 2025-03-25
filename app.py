c504be286d81e60094509acef16fa956da7b3319
sidpkul21 <141529348+sidpkul21@users.noreply.github.com>
2025-03-25 05:28:08 +0000

    added dynamic pages for each job

--- app.py
+++ app.py
@@ -1,19 +1,30 @@
 from flask import Flask, jsonify, render_template
-from database import load_jobs_from_db
+from database import load_jobs_from_db, load_job_from_db
 app = Flask(__name__)
 
+COMPANY_NAME = "Job"
 
 @app.route('/')
 def hello_job():
-  JOBS = load_jobs_from_db()
+  jobs = load_jobs_from_db()
   return render_template('home.html', 
-                         jobs=JOBS,
-                         company_name='Job')
+                         jobs=jobs,
+                         company_name=COMPANY_NAME)
 
 @app.route('/api/jobs')
 def list_jobs():
-  JOBS = load_jobs_from_db()
-  return jsonify(JOBS)
+  jobs = load_jobs_from_db()
+  return jsonify(jobs)
 
+@app.route('/job/<id>')
+def show_job(id):
+  job = load_job_from_db(id)
+  if not job:
+    return "Not Found", 404
+  else:
+    job = job[0]
+    return render_template('jobpage.html', job=job,
+                         company_name=COMPANY_NAME)
+  
 if __name__ == '__main__':
   app.run(host = '0.0.0.0', debug =True)
--- database.py
+++ database.py
@@ -1,15 +1,23 @@
 import os
 import requests
 
+SUPABASE_URL = os.getenv('SUPABASE_URL')
+SUPABASE_KEY = os.getenv('SUPABASE_KEY')
+headers = {
+    "apikey": SUPABASE_KEY,
+    "Authorization": f"Bearer {SUPABASE_KEY}"
+}
+
 def load_jobs_from_db():
-    SUPABASE_URL = os.getenv('SUPABASE_URL')
-    SUPABASE_KEY = os.getenv('SUPABASE_KEY')
-    headers = {
-        "apikey": SUPABASE_KEY,
-        "Authorization": f"Bearer {SUPABASE_KEY}"
-    }
     results = requests.get(f"{SUPABASE_URL}/rest/v1/jobs", headers=headers)
     jobs = []
     for row in results.json():
         jobs.append(row)
     return jobs
+
+def load_job_from_db(id):
+    url = f"{SUPABASE_URL}/rest/v1/jobs?id=eq.{id}"
+    results = requests.get(url, headers=headers)
+    job = results.json()
+    return job
+    
